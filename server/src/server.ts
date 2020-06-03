8498b06ecf03a9b2096122a107ec69e074d484f5
Inácio Régis Neto <inacioregisneto@gmail.com>
2020-06-03 20:46:48 -0300

    Testes com rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,29 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Teste')
+const users = [
+    'ana',
+    'bruno',
+    'cleiton'
+]
 
-    response.json([
-        'a','b','c'
-    ]);
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
 });
+
+app.get('/users/:id', (request,response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+    return response.json(user);
+})
+
+app.post('/users/', (request,response) => {
+    return response.json({name: 'inacio'});
+})
 
 app.listen(3333)
