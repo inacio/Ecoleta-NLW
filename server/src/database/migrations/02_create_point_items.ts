804e0fc7a93631d3e95f5d380359d8dfc80d4d83
Inácio Régis Neto <inacioregisneto@gmail.com>
2020-06-04 01:48:49 -0300

    Knex config. and creation Database, migrations and Seeds

---
+++ server/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,23 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('point_items',table => {
+        table.increments('id').primary();
+
+        table.integer('point_id')
+        .notNullable()
+        .references('id')
+        .inTable('idpoints');
+
+        table.integer('item_id')
+        .notNullable()
+        .references('id')
+        .inTable('items');
+        
+        table.integer('title').notNullable();
+    });   
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('point_items');
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,15 @@
 import express from 'express';
-
+import routes from './routes'
 const app = express();
+
+app.use(express.json());
+app.use(routes);
+
 
 const users = [
     'ana',
     'bruno',
     'cleiton'
 ]
-
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request,response) => {
-    const id = Number(request.params.id);
-    const user = users[id];
-    return response.json(user);
-})
-
-app.post('/users/', (request,response) => {
-    return response.json({name: 'inacio'});
-})
 
 app.listen(3333)
