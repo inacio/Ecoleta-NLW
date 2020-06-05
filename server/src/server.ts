006a1382d7f39c5c72962d23c19670b9e202eb89
Inácio Régis Neto <inacioregisneto@gmail.com>
2020-06-05 01:58:37 -0300

    aula 3 - Init project frontend with React

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
 import path from 'path';
+import routes from './routes';
 
 const app = express();
 
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Header from './Header';
+
+
 function App() {
+  const  [counter,setCounter] = useState(1);
+  function btnClick(){
+    setCounter(counter + 1);
+    console.log(counter);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Loading...</h1>
-      </header>
+    <div>
+      <Header title={`Contador: ${counter}`} />
+      <h1>{counter}</h1>
+      <button onClick={btnClick}>Increment</button>
     </div>
   );
 }
