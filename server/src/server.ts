4204741be1e2c1640a6443f13570a62ae8822c46
Inácio Régis Neto <inacioregisneto@gmail.com>
2020-06-04 23:40:21 -0300

    Using cors and update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
 const app = express();
 
+app.use(cors());//permit all requests
 app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
-
-const users = [
-    'ana',
-    'bruno',
-    'cleiton'
-]
 
 app.listen(3333)
