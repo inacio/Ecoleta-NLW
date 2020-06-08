6138de7e8fe3ba3ff88e35022153d8c67a501985
Inácio Régis Neto <inacioregisneto@gmail.com>
2020-06-07 23:04:19 -0300

    Update ip address

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,16 +1,18 @@
 import {Request, Response } from 'express';
 import knex from '../database/connection';
+import IP from 'ip';
 
 class ItemsController{
 
     async index(request: Request,response: Response) {
         let items = await knex('items').select('*');
+        let ip = (IP.address('Ethernet')) ? IP.address('Ethernet') : IP.address();
 
         items = items.map(item => {
             return { 
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://${ip}:3333/uploads/${item.image}`
             }
         })
 
